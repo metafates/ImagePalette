4b3c67fab21c6dc32e45a623064b7bceca0ffbfb
Lain <vvz.dev@gmail.com>
2022-01-19 18:16:41 +0300

    args added

---
+++ argsparser.py
@@ -0,0 +1,19 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog='waller',
+    description="Generates color palette from image"
+)
+
+parser.add_argument(
+    'image',
+    help='image to work with',
+    type=str
+)
+
+parser.add_argument(
+    '--colors',
+    help='how many colors to show',
+    type=int,
+    default=7
+)
--- main.py
+++ main.py
@@ -1,24 +1,32 @@
-from xml.etree.ElementTree import PI
 from colorthief import ColorThief
 from PIL import Image, ImageDraw, ImageFont, ImageEnhance, ImageFilter
 import os
 import math
+from argsparser import parser
 
-IMAGE_NAME = "img"
-IMAGE_EXTENSION = "jpg"
-COLOR_COUNT = 7  # minimum 2
+args = parser.parse_args()
+
+IMAGE_FULLNAME = args.image
+
+IMAGE_NAME = ''.join(IMAGE_FULLNAME.split('.')[:-1])
+IMAGE_EXTENSION = IMAGE_FULLNAME.split('.')[-1]
+COLOR_COUNT = args.colors
 BLOCK_GAP = .2  # %
 BLUR = .01  # %
 FONT_NAME = "JetBrainsMono.ttf"
 DIR = os.getcwd()
-IMAGE_PATH = os.path.join(DIR, IMAGE_NAME + '.' + IMAGE_EXTENSION)
-FONT_PATH = os.path.join(os.path.join(DIR, 'fonts'), FONT_NAME)
+IMAGE_PATH = os.path.join(DIR, IMAGE_FULLNAME)
+FONT_PATH = os.path.join(os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), 'fonts'), FONT_NAME)
 
 RGB = tuple[int, int, int]
 HEX = str
 
 
 def get_palette(image_path: str, count: int) -> list[RGB]:
+    '''
+    Generate a color pallette from the image
+    '''
     color_thief = ColorThief(image_path)
     palette: list[RGB] = color_thief.get_palette(color_count=count, quality=6)
     return palette
@@ -28,7 +36,10 @@ def rgb_to_hex(rgb: RGB) -> HEX:
     return '#%02x%02x%02x' % rgb
 
 
-def make_block_size(img_size: tuple[int, int]) -> tuple[int, int]:
+def calculate_block_size(img_size: tuple[int, int]) -> tuple[int, int]:
+    '''
+    Calculate blocks width and height based on image size
+    '''
     w, h = img_size
     block_h = h * .4
     block_w = w * .1
@@ -36,6 +47,9 @@ def make_block_size(img_size: tuple[int, int]) -> tuple[int, int]:
 
 
 def add_center_text(image: Image.Image, text: str):
+    '''
+    Add text to the image and align it
+    '''
     w, h = image.size
     font = ImageFont.truetype(FONT_PATH, int(w * .15))
     draw = ImageDraw.Draw(image)
@@ -43,13 +57,19 @@ def add_center_text(image: Image.Image, text: str):
 
 
 def make_block(color: RGB, size: tuple[int, int]) -> Image.Image:
+    '''
+    Make a color block
+    '''
     block = Image.new(mode="RGB", size=size, color=color)
     add_center_text(block, rgb_to_hex(color))
     return block
 
 
-# TODO: add shadows
+# TODO: add shadows maybe
 def add_blocks(image: Image.Image, blocks: list[Image.Image]):
+    '''
+    Add color blocks to the image
+    '''
     image_w, image_h = image.size
 
     blocks_len = sum(block.width for block in blocks)
@@ -66,6 +86,9 @@ def add_blocks(image: Image.Image, blocks: list[Image.Image]):
 
 
 def add_gaps(blocks: list[Image.Image]) -> list[Image.Image]:
+    '''
+    Insert empty blocks to make them serve as gaps
+    '''
     w = blocks[0].width
     gap = Image.new('RGBA', (int(w*BLOCK_GAP), 0), (0, 0, 0, 0))
     gap.putalpha(0)
@@ -76,15 +99,20 @@ def add_gaps(blocks: list[Image.Image]) -> list[Image.Image]:
     return new[:-1]
 
 
-with Image.open(IMAGE_PATH) as im:
-    palette = get_palette(IMAGE_PATH, COLOR_COUNT)
-    block_size = make_block_size(im.size)
-    blocks = [make_block(color, block_size) for color in palette]
-    blocks = add_gaps(blocks)
-    new_image = im.copy()
-    enhancer = ImageEnhance.Brightness(new_image)
-    new_image = enhancer.enhance(.9)
-    new_image = new_image.filter(
-        ImageFilter.BoxBlur(min(new_image.size) * BLUR))
-    add_blocks(new_image, blocks)
-    new_image.save(f'{IMAGE_NAME}-palette.{IMAGE_EXTENSION}')
+def main():
+    with Image.open(IMAGE_PATH) as im:
+        palette = get_palette(IMAGE_PATH, COLOR_COUNT)[:COLOR_COUNT]
+        block_size = calculate_block_size(im.size)
+        blocks = [make_block(color, block_size) for color in palette]
+        blocks = add_gaps(blocks)
+        new_image = im.copy()
+        enhancer = ImageEnhance.Brightness(new_image)
+        new_image = enhancer.enhance(.9)
+        new_image = new_image.filter(
+            ImageFilter.BoxBlur(min(new_image.size) * BLUR))
+        add_blocks(new_image, blocks)
+        new_image.save(f'{IMAGE_NAME}-palette.{IMAGE_EXTENSION}')
+
+
+if __name__ == "__main__":
+    main()
